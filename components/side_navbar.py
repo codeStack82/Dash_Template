cbd0355e9bbc9b9039c64ec0da1e80426f4bbf78
tyler <codestack82@gmail.com>
2020-03-07 12:09:30 -0600

    create temp file for side bar

--- components/side_navbar.py
+++ components/side_navbar.py
@@ -1,2 +1,13 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+
+
+def get_navbar():
+
+    # TODO: Build and save my own logo in assets folder
+    PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+
+    # TODO: build left nav bar code here
+    return dbc.Row([
+
+    ])
